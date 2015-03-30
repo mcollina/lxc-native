ea41ed6642f12f9c2f326c6c27533bb75b74ac8b
Matteo Collina <hello@matteocollina.com>
2015-03-30 14:57:12 +0200

    Removed wrapper.

--- lxc.js
+++ lxc.js
@@ -1,34 +1,3 @@
 var binding = require('bindings')('lxc.node').lxc
-var assert = require('assert')
 
-function Container (name) {
-  if (!(this instanceof Container)) {
-    return new Container(name)
-  }
-
-  assert(name, 'missing name')
-
-  this.binding = binding(name)
-}
-
-Container.prototype.isDefined = function (cb) {
-  this.binding.isDefined(cb)
-}
-
-Container.prototype.getPid = function (cb) {
-  this.binding.getPid(cb)
-}
-
-Container.prototype.getStatus = function (cb) {
-  this.binding.getStatus(cb)
-}
-
-Container.prototype.start = function (opts, cb) {
-  this.binding.start(opts, cb)
-}
-
-Container.prototype.create = function (opts, cb) {
-  this.binding.create(opts, cb)
-}
-
-module.exports = Container
+module.exports = binding;
